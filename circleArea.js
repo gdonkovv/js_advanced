1f7a0f2cf66da006d2e07a0479e012031484db4e
gdonkovv <g_donkov@abv.bg>
2022-05-22 14:14:44 +0300

    circleArea solution

---
+++ circleArea.js
@@ -0,0 +1,11 @@
+function circleArea(radius) {
+
+    if (typeof radius === "number") {
+        console.log((Math.PI * radius * radius).toFixed(2));
+    } else {
+        console.log(`We can not calculate the circle area, because we receive a ${typeof radius}.`);
+    }
+
+}
+circleArea(5);
+circleArea('name');

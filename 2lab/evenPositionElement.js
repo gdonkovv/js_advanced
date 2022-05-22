15e346d030bc31ba079b606be7c0c9f25d79becb
gdonkovv <g_donkov@abv.bg>
2022-05-22 17:13:01 +0300

    evenPositionElement solution

---
+++ 2lab/evenPositionElement.js
@@ -0,0 +1,9 @@
+function evenPositionElement(input) {
+
+    let result = [];
+    for (let i = 0; i < input.length; i += 2) {
+        result.push(input[i]);
+    }
+    console.log(result.join(" "));
+}
+evenPositionElement(['5', '10']);

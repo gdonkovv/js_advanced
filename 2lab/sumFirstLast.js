0a51482d49f2c0854e9c9343f23d9af9dc64c7ff
gdonkovv <g_donkov@abv.bg>
2022-05-22 17:26:41 +0300

    sumFirstLast solution

---
+++ 2lab/sumFirstLast.js
@@ -0,0 +1,10 @@
+function sumFirstLast(input) {
+
+    let first = Number(input[0]);
+    let last = Number(input[input.length - 1]);
+
+    console.log(first + last);
+
+}
+sumFirstLast(['20', '30', '40']);
+sumFirstLast(['5', '10']);

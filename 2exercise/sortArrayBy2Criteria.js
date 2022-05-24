f9bb2a7f49701a6c498fcd1f51271a3eb779a311
gdonkovv <g_donkov@abv.bg>
2022-05-24 14:29:01 +0300

    sortArrayBy2Criteria solution

---
+++ 2exercise/sortArrayBy2Criteria.js
@@ -0,0 +1,15 @@
+function sortArrayBy2Criteria(input) {
+
+    input.sort((a, b) => {
+        if (a.length !== b.length) {
+            return a.length - b.length;
+        } else {
+            return a.localeCompare(b);
+        }
+    });
+    console.log(input.join("\n"));
+
+
+}
+sortArrayBy2Criteria(['alpha', 'beta', 'gamma']);
+sortArrayBy2Criteria(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']);

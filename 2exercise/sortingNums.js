93308cc3c05fdffe2a1657117d95c19feeb29bc2
gdonkovv <g_donkov@abv.bg>
2022-05-24 14:23:17 +0300

    sortingNums solution

---
+++ 2exercise/sortingNums.js
@@ -0,0 +1,10 @@
+function sortingNums(input) {
+    let result = [];
+    input.sort((a, b) => a - b);
+    while (input.length > 0) {
+        result.push(input.shift());
+        result.push(input.pop());
+    }
+    return result;
+}
+sortingNums([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);

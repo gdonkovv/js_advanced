b7c45d39b4429f65e8ee12d819de130bc4b8a6bf
gdonkovv <g_donkov@abv.bg>
2022-05-29 20:46:29 +0300

    heroes solution

---
+++ 3exercise/heroes.js
@@ -0,0 +1,44 @@
+function solve() {
+
+    let result = {
+        mage,
+        fighter
+    };
+
+    function mage(name) {
+        return {
+            name,
+            health: 100,
+            mana: 100,
+            cast: function (spell) {
+                this.mana--;
+                console.log(`${this.name} cast ${spell}`);
+            }
+        }
+    }
+
+    function fighter(name) {
+        return {
+            name,
+            health: 100,
+            stamina: 100,
+            fight: function () {
+                this.stamina--;
+                console.log(`${this.name} slashes at the foe!`);
+            }
+        }
+    }
+
+    return result;
+}
+let create = solve();
+const scorcher = create.mage("Scorcher");
+scorcher.cast("fireball")
+scorcher.cast("thunder")
+scorcher.cast("light")
+
+const scorcher2 = create.fighter("Scorcher 2");
+scorcher2.fight()
+
+console.log(scorcher2.stamina);
+console.log(scorcher.mana);
